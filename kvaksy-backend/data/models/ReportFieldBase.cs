93c8945c454dcf3d0270a9fbaaa1866f6bf39705
phil2988 <phillipblicher@gmail.com>
2024-03-03 11:22:49 +0100

    Improve web interface (#4)
    * updated information
    
    * updated controller return types
    
    * updated launch url
    
    * forgot to change branch...
    
    ---------
    
    Co-authored-by: Phillip Blicher <pfbb@blackcapitaltechnology.com>

---
+++ kvaksy-backend/data/models/ReportFieldBase.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.OpenApi.Any;
+
+namespace kvaksy_backend.data.models
+{
+    public abstract class ReportFieldBase
+    {
+        
+    }
+}
---
+++ kvaksy-backend/data/models/TemperatureField.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace kvaksy_backend.data.models
+{
+    public class TemperatureField: ReportFieldBase
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public double Temperature { get; set; }
+    }
+}
